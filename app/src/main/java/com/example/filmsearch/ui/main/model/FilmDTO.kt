3ccca3ab1a32f3ffeb78738453dc8fe7bc339533
MariaDemidova <demidova.ma@yandex.ru>
2021-09-17 20:03:34 +0300

    hw5

--- app/src/main/java/com/example/filmsearch/ui/main/model/FilmDTO.kt
+++ app/src/main/java/com/example/filmsearch/ui/main/model/FilmDTO.kt
@@ -1,4 +1,16 @@
 package com.example.filmsearch.ui.main.model
 
-class FilmDTO {
+data class FilmDTO(
+    val genres: List<Genres>,
+    val title: String?,
+    val id: Long?,
+    val original_language: String?,
+    val release_date: String?,
+    val overview: String?,
+    val poster_path: String?,
+    ) {
+    data class Genres(
+        val id: Long?,
+        val name: String?
+    )
 }
--- app/src/main/java/com/example/filmsearch/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/filmsearch/ui/main/view/MainActivity.kt
@@ -7,7 +7,6 @@ import com.example.filmsearch.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     private val binding: MainActivityBinding by lazy {
         MainActivityBinding.inflate(layoutInflater)
     }
